d193129b844fc8b77d939bafda89f74dcd22e628
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-09-10 15:59:44 +0100

    feat: implementing the logic for the chat and messagging route

--- api/routes/chat.route.js
+++ api/routes/chat.route.js
@@ -1,23 +1,17 @@
 import express from "express";
 import {
-  deleteUser,
-  getUser,
-  getUsers,
-  updateUser,
-  savePost,
-  profilePosts,
-  getNotificationNumber,
-} from "../controllers/user.controller.js";
+  getChats,
+  getChat,
+  addChat,
+  readChat,
+} from "../controllers/chat.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-// router.get("/search/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", verifyToken, deleteUser);
-router.post("/save", verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
-router.get("/notification", verifyToken, getNotificationNumber);
+router.get("/", verifyToken, getChats);
+router.get("/:id", verifyToken, getChat);
+router.post("/", verifyToken, addChat);
+router.put("/read/:id", verifyToken, readChat);
 
 export default router;
